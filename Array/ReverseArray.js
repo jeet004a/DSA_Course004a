8cac6a6d8cb0ecb0fe6e1319843f425468a2aa1d
jeet <wastijeet110@gmail.com>
2025-02-11 21:37:21 +0530

    How to reverse an array

---
+++ Array/ReverseArray.js
@@ -0,0 +1,18 @@
+//How to reverse an array
+function check(a) {
+    let n = a.length
+
+    // console.log(Math.floor(n / 2))
+
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        [a[i], a[n - i - 1]] = [a[n - i - 1], a[i]]
+    }
+
+    console.log(a)
+}
+
+
+
+let a = [1, 2, 3, 4, 5]
+
+check(a)
