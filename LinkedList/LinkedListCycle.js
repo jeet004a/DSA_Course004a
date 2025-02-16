2909b6fa2695ca89759d87e0c81df6b0bbf15954
jeet <wastijeet110@gmail.com>
2025-02-16 19:59:20 +0530

    Detetct Loop in a linkedlist

---
+++ LinkedList/LinkedListCycle.js
@@ -0,0 +1,16 @@
+//Detect Loop in a linkedlist
+function checkCycle = (head) => {
+    let slow = head
+    let fast = head
+
+    while (fast != null && fast.next != null) {
+        slow = slow.next
+        fast = fast.next.next
+
+        if (slow == fast) {
+            return true
+        }
+    }
+
+    return false
+}
