67de71b846db3b964a33b6694b03389778df7be4
jeet <wastijeet110@gmail.com>
2025-02-20 00:22:22 +0530

    Added 3 sum problem leetcode 15

---
+++ Array/3Sum.js
@@ -0,0 +1,29 @@
+function check(nums) {
+    let ans = []
+    nums = nums.sort((a, b) => (a - b))
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue
+        let j = i + 1
+        let k = nums.length - 1
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k]
+            if (sum == 0) {
+                ans.push([nums[i], nums[j], nums[k]])
+
+                while (nums[j] == nums[j + 1]) j++
+                    while (nums[k] == nums[k + 1]) k--
+                        j++
+                        k--
+            } else if (sum < 0) {
+                j++
+            } else {
+                k--
+            }
+        }
+    }
+    console.log(ans)
+};
+
+let nums = [-1, 0, 1, 2, -1, -4]
+
+check(nums)
