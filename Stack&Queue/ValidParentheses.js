cb36d3c5abd2aa03cebd9d711f6aa9e3d88ce62f
jeet <wastijeet110@gmail.com>
2025-02-22 21:43:40 +0530

    Added Valid Parentheses

---
+++ Stack&Queue/ValidParentheses.js
@@ -0,0 +1,30 @@
+function ValidPan(s) {
+    let x = []
+    if (s.length < 2) return false
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            x.push(s[i])
+        } else {
+            let m = x.pop()
+            if (s[i] == ')' && m == '(') {
+                continue
+            } else if (s[i] == ']' && m == '[') {
+                continue
+            } else if (s[i] == '}' && m == '{') {
+                continue
+            } else {
+                return false
+            }
+        }
+    }
+
+    if (x.length == 0) {
+        return true
+    }
+    return false
+}
+
+
+let s = '()[]{}'
+
+console.log(ValidPan(s))
