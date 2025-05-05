8cc323fd8427f543cb505949c29b49cf3e778fdf
jeet <wastijeet110@gmail.com>
2025-05-05 23:44:36 +0530

    Added Graph question

--- DSAInterview/String/test.js
+++ DSAInterview/String/test.js
@@ -1,8 +1,31 @@
 function check(s, k) {
+    let i = 0
+    let m = []
+    let x = 0
+        // k = k - 1
+    for (let j = 0; j < s.length; j++) {
+        if (k > 0) {
+            if (m.includes(s[j])) {
+                m.push(s[j])
+                k--
+            } else {
+                m.push(s[j])
 
+            }
+        } else {
+            while (k <= 0) {
+                m.shift()
+                k++
+                i++
+            }
+        }
+        x = Math.max(x, m.length)
+    }
+
+    console.log(x)
 }
-
-// s = "aabacbebebe"
-s = "abcd"
-k = 2
+// s = "aabacbebebe", k = 3
+s = "aabaaab", k = 2
+    // s = "abcd", k = 2
+    // k = 3
 check(s, k)
