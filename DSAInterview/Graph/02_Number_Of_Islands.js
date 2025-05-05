0ca55a4a41795311494953eb112ffdb162aed105
jeet <wastijeet110@gmail.com>
2025-05-05 23:48:00 +0530

    Added number of islands

---
+++ DSAInterview/Graph/02_Number_Of_Islands.js
@@ -0,0 +1,24 @@
+function dfs(i, j, grid) {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+        return
+    }
+    grid[i][j] = "0"
+    dfs(i + 1, j, grid)
+    dfs(i - 1, j, grid)
+    dfs(i, j + 1, grid)
+    dfs(i, j - 1, grid)
+}
+
+var numIslands = function(grid) {
+    let count = 0
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] == '1') {
+                dfs(i, j, grid)
+                count++
+            }
+        }
+    }
+
+    return count
+};
