db649771a762519d1907ba46ceeb9abf98e7539a
jeet <wastijeet110@gmail.com>
2025-05-23 02:03:48 +0530

    Added leetocde 108

---
+++ DSAInterview/Trees/14_Converted_Array_To_Binary_Tree.js
@@ -0,0 +1,9 @@
+// 108. Leetcode COnverted sorted array to binary search tree
+var sortedArrayToBST = function(nums) {
+    if (!nums.length) return null
+    let mid = Math.floor(nums.length / 2)
+    let node = new TreeNode(nums[mid])
+    node.left = sortedArrayToBST(nums.slice(0, mid))
+    node.right = sortedArrayToBST(nums.slice(mid + 1))
+    return node
+};
