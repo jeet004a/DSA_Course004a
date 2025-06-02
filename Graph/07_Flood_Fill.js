046a763808229afe66313cb6a624c069530ca025
jeet <wastijeet110@gmail.com>
2025-06-02 23:26:28 +0530

    Added flood fill

---
+++ Graph/07_Flood_Fill.js
@@ -0,0 +1,20 @@
+function dfs(image, i, j, oldColor, color) {
+    if (i < 0 || j < 0 || i >= image.length || j >= image[0].length || image[i][j] != oldColor) {
+        return
+    }
+    image[i][j] = color
+    dfs(image, i + 1, j, oldColor, color)
+    dfs(image, i - 1, j, oldColor, color)
+    dfs(image, i, j + 1, oldColor, color)
+    dfs(image, i, j - 1, oldColor, color)
+}
+
+var floodFill = function(image, sr, sc, color) {
+    let oldColor = image[sr][sc]
+    if (oldColor == color) {
+        return image
+    }
+    dfs(image, sr, sc, oldColor, color)
+
+    return image
+};
