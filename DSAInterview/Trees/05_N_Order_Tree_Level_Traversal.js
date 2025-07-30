67fb9a9f8b1bef47c97ea5c3e35343c25b716dbf
jeet <wastijeet110@gmail.com>
2025-07-30 19:11:59 +0530

    added one more solution to N Order Tree level traversal

--- DSAInterview/Trees/05_N_Order_Tree_Level_Traversal.js
+++ DSAInterview/Trees/05_N_Order_Tree_Level_Traversal.js
@@ -36,4 +36,32 @@ var levelOrder = function(root) {
         result.push(b)
     }
     return result
+}
+
+//Below is one more solution
+/**
+ * // Definition for a _Node.
+ * function _Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+var levelOrder = function(root) {
+    if (!root) return []
+    let q = [root]
+    result = []
+    while (q.length) {
+        let l = q.length
+        result.push(q.map((data) => data.val))
+        while (l > 0) {
+            let node = q.shift()
+            if (node.children) {
+                q.push(...node.children) //This process similar like if we have left or right. In this scenerio we have childer that is why we use spread operator to push all the childern to current q
+            }
+            l--
+        }
+    }
+
+    return result
 };
