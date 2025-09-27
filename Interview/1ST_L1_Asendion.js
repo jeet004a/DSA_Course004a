d3446c04e6933366b598b736d744d87f46f62ddb
jeet <wastijeet110@gmail.com>
2025-09-27 16:41:00 +0530

    Added Assendion L1 Interview

---
+++ Interview/1ST_L1_Asendion.js
@@ -0,0 +1,5 @@
+// L1 Interview in Asendion 
+
+// All question about Nodejs stuff.
+
+//Please refer folder ./Node/Ascedion Interiew
--- Node_K8s_Deployment/Node_K8s_Deployment.js
+++ Node_K8s_Deployment/Node_K8s_Deployment.js
@@ -3,7 +3,14 @@ Step 1.
 Create a basic node application or based on your requirement
 
 
-Step
+Step 2.
+Then create a docker file. Please refere the DockerFile in the directory. Create a docker image from it.
+
+Step 3. 
+Then create a deployment.yaml file. Please refere the Deployment.yaml file
+
+Step 4.
+Then create a ingress.yaml file with your created service. Please refere ingress.yaml file
 
 
 /*
