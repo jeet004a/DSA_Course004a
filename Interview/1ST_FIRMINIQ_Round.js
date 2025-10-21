56de7d6483c2f2299c97ad338286beed0d64da3f
jeet <wastijeet110@gmail.com>
2025-10-21 19:52:23 +0530

    added one question

--- Interview/1ST_FIRMINIQ_Round.js
+++ Interview/1ST_FIRMINIQ_Round.js
@@ -19,9 +19,42 @@ example
 Hello 1
 Hello 2
 Hello 3
+*/
+function check() {
+    return new Promise((resolve, reject) => {
+        reject('hello')
+    })
+}
+
+let tryc = 3
+let maxTry = 1
+
+async function abc(fnc, delay) {
+    let timer
+    if (maxTry <= tryc) {
+        fnc().then((data) => {
+            console.log(data)
+        }).catch(() => {
+            setTimeout(() => {
+                console.log('Hello', maxTry)
+                maxTry++
+                abc(fnc, delay)
+            }, 2000)
+        })
+    }
+
+
+}
+
+abc(check, 3000)
 
 
 
+
+
+
+
+/*
 Q5. Create request pipeline that have one bussiness logic function and before sending the response to the user add a key value paire on it
 
 Q6. Diffrence between find and aggregate.
