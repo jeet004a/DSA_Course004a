3ee5ea31bdcf3bc9042f20734e295df264bac7a8
jeet <wastijeet110@gmail.com>
2025-11-28 15:19:48 +0530

    Added 1st round of Innovate labs interview question

---
+++ Interview/1ST_Inovate_Labs.js
@@ -0,0 +1,16 @@
+//Q1. Design Netflix User table with minimal constrains
+
+//Q2. Explain the Event Loop Architecture
+
+//Q3. Hacker Rank:- Sparse Arrays 
+/*
+stringList=['ab','ab','abc']
+queries=['ab','abc','bc']
+
+Result=[2,1,0]
+
+*/
+
+//Q4. How do you deploy a backend application in AWS specilly
+
+//Q5. Explain one of the Microservice Architectue you previously worked on.
