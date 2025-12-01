5476cc683926ce4f2b6b2761d9e21078f4bdb2f3
jeet <wastijeet110@gmail.com>
2025-12-01 21:09:57 +0530

    Added Number of islands solution for GFG only

--- DSAInterview/Graph/02_Number_Of_Islands.js
+++ DSAInterview/Graph/02_Number_Of_Islands.js
@@ -1,3 +1,4 @@
+//Below code is 4 directions only (Leetcode. 200 Soluion.)
 function dfs(i, j, grid) {
     if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
         return
@@ -22,3 +23,51 @@ var numIslands = function(grid) {
 
     return count
 };
+
+//Below code is for 8 directions (GFG solution)
+class Solution {
+    numIslands(grid) {
+        // code here
+
+        const dirs = [
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, -1],
+            [0, 1],
+            [1, -1],
+            [1, 0],
+            [1, 1]
+        ];
+        let solve = (i, j, grid) => {
+            if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 'W') {
+                return
+            }
+
+            grid[i][j] = 'W'
+
+            // solve(i,j+1,grid)
+            // solve(i,j-1,grid)
+            // solve(i+1,j,grid)
+            // solve(i-1,j,grid)
+
+            for (let [x, y] of dirs) {
+                solve(i + x, j + y, grid)
+            }
+        }
+
+
+        let count = 0
+
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 'L') {
+                    solve(i, j, grid)
+                    count++
+                }
+            }
+        }
+
+        return count
+    }
+}
