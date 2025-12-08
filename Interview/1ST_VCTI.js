cca62c22e58eb29ec8275100026a53aaeb9166c4
jeet <wastijeet110@gmail.com>
2025-12-08 21:27:42 +0530

    Added VCTI 1st round interview

---
+++ Interview/1ST_VCTI.js
@@ -0,0 +1,34 @@
+// Diffreces between Var Let and Cost
+
+// Explain the concent of Hoisting
+
+// What is promises and Async await
+
+// What are arrow functions
+
+// What is clousers and explain a real life example
+
+
+// Q1. Write a code that will idenyify the missing number from given list
+//a=[1,2,3,5]
+
+// Write a code that will print if number is divisible by 3 then print 'Hi' if the number is divisable by 5 then print 'Hello' if both then print 'Hi Hello' else print number only
+//Contraints number is starting from 1, 20
+
+
+/*
+Please guess the output of below code
+console.log('A')
+setTimeout(() => {
+    console.log('B')
+}, 0)
+
+Promise.resolve().then(()=>console.log('C'))
+console.log('D')
+
+Output
+A
+D
+C
+B
+*/
